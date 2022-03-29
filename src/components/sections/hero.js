f0318974cd56e4cebfbe80808942c840a847f009
Warrie Warrie <warrie.warrieus@gmail.com>
2022-03-29 06:36:54 +0100

    fixed-typos

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,7 +60,7 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Warrie Warrie.</h2>;
-  const three = <h3 className="big-heading">I Build Data-Driven and Geospatial Technology.</h3>;
+  const three = <h3 className="big-heading">I Build Data-Driven and Geospatial Product.</h3>;
   const four = (
     <>
       <p>
